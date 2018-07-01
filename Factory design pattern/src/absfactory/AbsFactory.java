f4d1fa3f9cc7dd2cf41b25254270794da53c8a7e
amoshcxy <932142198@qq.com>
2018-07-01 18:16:14 +0800

    add all files

--- Factory design pattern/src/absfactory/AbsFactory.java
+++
@@ -1,7 +0,0 @@
-package absfactory;
-
-import pizza.Pizza;
-
-public interface AbsFactory {
-	public Pizza CreatePizza(String ordertype) ;
-}
--- Factory method model/src/Extension_of_the_factory_method_pattern/Multiple_factory_model/whiteHumanFactory.java
+++ Factory method model/src/Extension_of_the_factory_method_pattern/Multiple_factory_model/whiteHumanFactory.java
@@ -1,7 +1,13 @@
 package Extension_of_the_factory_method_pattern.Multiple_factory_model;
 
+import Nuwa_created_man.Human;
+import Nuwa_created_man.WhiteHuman;
+
 /**
  * Created by cxy on 2018/7/1.
  */
-public class whiteHumanFactory {
+public class WhiteHumanFactory extends AbstractHumanFactory {
+    public Human createHuman(){
+        return new WhiteHuman();
+    }
 }
